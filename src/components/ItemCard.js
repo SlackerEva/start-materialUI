7e587d24c0b1b69651681cf8d1d6ed322c4d6b1c
SlackerEva <Mariacros@mail.ru>
2021-12-10 19:32:28 +0300

    feat: Add header and body with cards

---
+++ src/components/ItemCard.js
@@ -0,0 +1,39 @@
+import Card from '@mui/material/Card';
+import Avatar from '@mui/material/Avatar';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import CardActions from '@mui/material/CardActions';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import StarIcon from '@mui/icons-material/StarBorderPurple500Sharp';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Typography from '@mui/material/Typography';
+import makeupArr from '../data/makeup.js';
+
+export default function ItemCard(props) {
+  return (
+    <Card>
+      <CardMedia
+        component='img'
+        image={props.url}
+        alt={props.name}
+      />
+      <CardHeader 
+        title={        
+          <Typography noWrap gutterBottom variant='h6' component='h4' textAlign='center'>
+            {props.name}
+          </Typography>
+        } 
+        sx={{display: 'block', borderBottom: '1px solid gray' }}
+      />
+      <CardActions disableSpacing>
+        <IconButton aria-label='add to favorites'>
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label='already bought'>
+          <StarIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
